606d09ff925479ede5e4ee9e7e6c065c79faf27b
Ajay Chenampara <ajay.chenampara@gmail.com>
2017-12-05 21:11:10 -0500

    Day 3 part 2

--- 03_day3.py
+++ 03_day3.py
@@ -1,3 +1,5 @@
+import itertools
+RESULT = {'[0, 0]': 1}
 
 def change_direction(current_direction):
      if current_direction == [ 1, 0 ]:
@@ -10,7 +12,7 @@ def change_direction(current_direction):
          return [ 1, 0 ]
 
 
-def one_iteration(val, input_data, steps, direction, position):          
+def one_iteration(val, input_data, steps, direction, position):
      i = 0
      while i < 2:  #  Each step value is relevant only twice (each traversal of the side of the sq)
          i += 1
@@ -21,11 +23,44 @@ def one_iteration(val, input_data, steps, direction, position):
              if val == input_data:
                  return True, val, direction, position
              steps -= 1
-         direction = change_direction(direction)  # change direction by 90 degrees 
+         direction = change_direction(direction)  # change direction by 90 degrees
          steps = s
      steps = steps + 1  # Increment steps and then recursively call the function
      return(one_iteration(val, input_data, steps,  direction, position))
 
+
+# PART 2
+
+def get_neighbor_positions(position):
+    x_axis = [1, -1, 0]
+    y_axis = [1, -1, 0]
+    for possible_position in itertools.product(x_axis, y_axis):
+        if possible_position != (0, 0):
+            neighbor_position = [x + y for x, y in zip(
+                position, list(possible_position))]
+            yield neighbor_position
+
+
+def find_closest_value(input_data, steps, direction, position):
+    i = 0
+    global RESULT
+    while i < 2:
+        i += 1
+        s = steps
+        while steps != 0:
+            val = 0
+            position = [x + y for x, y in zip(position, direction)]
+            for neighbor_position in get_neighbor_positions(position):
+                if RESULT.get(str(neighbor_position)):
+                    val = val + RESULT.get(str(neighbor_position))
+            RESULT[str(position)] = val
+            if val > input_data:
+                return val
+            steps -= 1
+        direction = change_direction(direction)
+        steps = s
+    steps += 1
+    return(find_closest_value(input_data, steps, direction, position))
 
 if __name__ == "__main__":
     input_data = 277678
@@ -37,3 +72,10 @@ if __name__ == "__main__":
     result_position = one_iteration(val, input_data, steps, direction, position)[-1]
     distance = sum((abs(i) for i in result_position))
     print(distance)  # Expected 475
+    # PART 2
+    input_data = 277678
+    steps = 1
+    direction = [1, 0]
+    position = [0, 0]
+    closest_number = find_closest_value(input_data, steps, direction, position)
+    print(closest_number)  # Expected 279138
